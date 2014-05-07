9a6382b51118cf21d7cb279e81ccee3fa600d786
Paolo Ripamonti <p.ripamonti@lutech.it>
2014-05-07 11:45:45 +0200

    Upgrade route fro Rails 4

--- lib/devise_cas_authenticatable/routes.rb
+++ lib/devise_cas_authenticatable/routes.rb
@@ -1,10 +1,12 @@
-if ActionController::Routing.name =~ /ActionDispatch/
-  # Rails 3
+if defined? ActionDispatch::Routing
+  # Rails 3, 4
   
   ActionDispatch::Routing::Mapper.class_eval do
     protected
   
     def devise_cas_authenticatable(mapping, controllers)
+      sign_out_via = (Devise.respond_to?(:sign_out_via) && Devise.sign_out_via) || [:get, :post]
+
       # service endpoint for CAS server
       get "service", :to => "#{controllers[:cas_sessions]}#service", :as => "service"
       post "service", :to => "#{controllers[:cas_sessions]}#single_sign_out", :as => "single_sign_out"
@@ -13,8 +15,8 @@ if ActionController::Routing.name =~ /ActionDispatch/
         get :new, :path => mapping.path_names[:sign_in], :as => "new"
         get :unregistered
         post :create, :path => mapping.path_names[:sign_in]
-        match :destroy, :path => mapping.path_names[:sign_out], :as => "destroy"
-      end      
+        match :destroy, :path => mapping.path_names[:sign_out], :as => "destroy", :via => sign_out_via
+      end
     end
   end
 else
